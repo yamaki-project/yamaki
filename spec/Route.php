d19f2a08ba05a835d7c22074ef0ce5455485431b
kashimoo <kashimama3hiro@gmail.com>
2013-01-12 21:37:56 +0900

    Root.php initial commit

---
+++ spec/Route.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace spec;
+
+use PHPSpec2\ObjectBehavior;
+
+class Route extends ObjectBehavior
+{
+    function it_should_be_initializable()
+    {
+        $this->shouldHaveType('Route');
+    }
+}
