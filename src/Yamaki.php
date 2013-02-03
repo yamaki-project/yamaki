86adef203b6b7a1f976e5ddbd9dcbec352d684ad
Freddie Fujiwara <github@ze.gs>
2013-02-03 22:54:50 +0900

    add Yamaki::post

--- src/Yamaki.php
+++ src/Yamaki.php
@@ -10,18 +10,42 @@ class Yamaki
             self::$instance : 
             self::$instance = new self();
     }
-    public function get($rule,$function)
+
+    public static function clear()
     {
+        Yamaki\Router::clear();
+        return isset(self::$instance) ? 
+            self::$instance = null : null;
+    }
+
+    public function get($rule,$function,$default = null ){
+        $default = is_null($default) ? 
+                   function($route,$input){echo "404 Not Found\n";} :
+                   $default;
         $router =  Yamaki\Router::generate()
                 -> input(\Yamaki\Input::generate())
                 -> defaultRoute(\Yamaki\Route::generate()
-                    -> callback(function($route,$input){
-                            echo "404 Not Found\n";
-                        })
+                    -> callback($default)
                    )
                 -> put(\Yamaki\Route::generate()
                     -> rule($rule)
                     -> viaGet()
+                    -> callback($function))
+                -> dispatch();
+    }
+
+    public function post($rule,$function,$default = null ){
+        $default = is_null($default) ? 
+                   function($route,$input){echo "404 Not Found\n";} :
+                   $default;
+        $router =  Yamaki\Router::generate()
+                -> input(\Yamaki\Input::generate())
+                -> defaultRoute(\Yamaki\Route::generate()
+                    -> callback($default)
+                   )
+                -> put(\Yamaki\Route::generate()
+                    -> rule($rule)
+                    -> viaPost()
                     -> callback($function))
                 -> dispatch();
     }
--- test/Yamaki/YamakiTest.php
+++ test/Yamaki/YamakiTest.php
@@ -3,19 +3,30 @@ class YamakiTest extends \PHPUnit_Framework_TestCase
 {
     private $obj = null;
     function setUp(){
+        \Yamaki::clear();
         $this -> obj = \Yamaki::generate();
     }
     function testGenerate(){
         $this->assertEquals('Yamaki', get_class($this -> obj));
+    }
+    function testClear(){
+        $this->assertEquals('Yamaki', get_class($this -> obj));
+        $this->assertNull(\Yamaki::clear());
     }
 
     function testGetRequestForDefault()
     {
         $_SERVER['REQUEST_URI'] = "/yamaki/12345678/12345678/" ;
         $_SERVER['REQUEST_METHOD'] = 'POST';
-        $this -> obj -> get("/yamaki/:ichiban/:niban/:sanban",function($route,$input){
-            $this -> assertEquals(1,1);
-        });
+        try{
+            $this -> obj -> get("/yamaki/:ichiban/:niban",function($route,$input){
+                throw new Exception($route -> param('ichiban').'/'.$route -> param('niban'));
+            },function($route,$input){
+                throw new Exception("default");
+            });
+        }catch(\Exception $e){
+            $this -> assertEquals('default',$e -> getMessage());
+        }
     }
 
     function testGetRequest()
@@ -24,6 +35,33 @@ class YamakiTest extends \PHPUnit_Framework_TestCase
         $_SERVER['REQUEST_METHOD'] = 'GET';
         try{
             $this -> obj -> get("/yamaki/:ichiban/:niban",function($route,$input){
+                throw new Exception($route -> param('ichiban').'/'.$route -> param('niban'));
+            });
+        }catch(\Exception $e){
+            $this -> assertEquals('12345678/87654321',$e -> getMessage());
+        }
+   }
+    function testPostRequestForDefault()
+    {
+        $_SERVER['REQUEST_URI'] = "/yamaki/12345678/12345678/" ;
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        try{
+            $this -> obj -> post("/yamaki/:ichiban/:niban",function($route,$input){
+                throw new Exception($route -> param('ichiban').'/'.$route -> param('niban'));
+            },function($route,$input){
+                throw new Exception("default");
+            });
+        }catch(\Exception $e){
+            $this -> assertEquals('default',$e -> getMessage());
+        }
+    }
+
+    function testPostRequest()
+    {
+        $_SERVER['REQUEST_URI'] = "/yamaki/12345678/87654321/" ;
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        try{
+            $this -> obj -> post("/yamaki/:ichiban/:niban",function($route,$input){
                 throw new Exception($route -> param('ichiban').'/'.$route -> param('niban'));
             });
         }catch(\Exception $e){
